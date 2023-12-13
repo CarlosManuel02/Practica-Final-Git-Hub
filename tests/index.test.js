0f54fd2d19e52e866174db6e54e4e47851695f15
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2023-12-13 09:19:05 -0400

    refactor

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const obtenerSaludo  = require('./src/main');
+const obtenerSaludo  = require('../src/index');
 
 test('hola_mundo function should return "Hola Mundo"', () => {
     expect(obtenerSaludo()).toBe('Practica Final!, Carlos Manuel Cedeño Barrera 2022-0018!');
